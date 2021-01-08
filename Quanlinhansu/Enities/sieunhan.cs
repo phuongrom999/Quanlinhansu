bb1b8f2aaa05c23ea4a02b0e7078f431e51c8f00
ngoc nguyen <nguyenhongngoc1209@gmail.com>
2021-01-08 23:46:21 +0700

    dô nek

---
+++ Quanlinhansu/Enities/sieunhan.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quanlinhansu.Enities
+{
+    class sieunhan
+    {
+        public string sieunhan1 { get; set; }
+    }
+}
