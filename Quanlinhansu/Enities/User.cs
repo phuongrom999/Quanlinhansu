e999f51089f8daeb85ccbd999e62c2e8d2c6170e
ngoc nguyen <nguyenhongngoc1209@gmail.com>
2021-01-08 22:17:15 +0700

    moi them do

---
+++ Quanlinhansu/Enities/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quanlinhansu.Enities
+{
+    public class User
+    {
+        public static int ID { get; set; }
+
+        public static string HoTen { get; set; }
+    }
+}
